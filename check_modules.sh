840a10005692fe55a6151ce128678aaef32a468e
BL <borek.latinovic@powerdynax.com>
2024-04-06 01:58:47 +0200

    Some extra stuff

---
+++ check_modules.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+grep '\<CONFIG_XFRM_USER\>' /proc/config.gz``
+grep '\<CONFIG_NET_KEY\>' /proc/config.gz``
+grep '\<CONFIG_INET\>' /proc/config.gz``
+grep '\<CONFIG_IP_ADVANCED_ROUTER\>' /proc/config.gz``
+grep '\<CONFIG_IP_MULTIPLE_TABLES\>' /proc/config.gz``
+grep '\<CONFIG_INET_AH\>' /proc/config.gz``
+grep '\<CONFIG_INET_ESP\>' /proc/config.gz``
+grep '\<CONFIG_INET_IPCOMP\>' /proc/config.gz``
+grep '\<CONFIG_INET_XFRM_MODE_TRANSPORT\>' /proc/config.gz``
+grep '\<CONFIG_INET_XFRM_MODE_TUNNEL\>' /proc/config.gz``
+grep '\<CONFIG_INET_XFRM_MODE_BEET\>' /proc/config.gz``
+grep '\<CONFIG_IPV6\>' /proc/config.gz``
+grep '\<CONFIG_INET6_AH\>' /proc/config.gz``
+grep '\<CONFIG_INET6_ESP\>' /proc/config.gz``
+grep '\<CONFIG_INET6_IPCOMP\>' /proc/config.gz``
+grep '\<CONFIG_INET6_XFRM_MODE_TRANSPORT\>' /proc/config.gz``
+grep '\<CONFIG_INET6_XFRM_MODE_TUNNEL\>' /proc/config.gz``
+grep '\<CONFIG_INET6_XFRM_MODE_BEET\>' /proc/config.gz``
+grep '\<CONFIG_IPV6_MULTIPLE_TABLES\>' /proc/config.gz``
+grep '\<CONFIG_NETFILTER\>' /proc/config.gz``
+grep '\<CONFIG_NETFILTER_XTABLES\>' /proc/config.gz``
+grep '\<CONFIG_NETFILTER_XT_MATCH_POLICY\>' /proc/config.gz``
